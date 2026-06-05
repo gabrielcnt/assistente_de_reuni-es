8d8f170de95f678aec7180149ea2939fedba8075
Gabriel Vieira <gabrielcnt96@gmail.com>
2026-06-05 15:02:10 -0300

    feat: implementa transcrição

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 from pathlib import Path
 
+from openai import OpenAI
+
+from config import OPENAI_API_KEY
+
+client = OpenAI(api_key=OPENAI_API_KEY)
+
 AUDIO_DIR = Path("audio")
 
 
@@ -16,4 +22,29 @@ def encontrar_audio():
 
     return audio
 
-encontrar_audio()
+
+def transcrever_audio(caminho_audio):
+    print("Transcrevendo audio...")
+
+    with open(caminho_audio, "rb") as audio:
+        transcript = client.audio.transcriptions.create(
+            model="gpt-4o-mini-transcribe", file=audio
+        )
+
+    texto = transcript.text
+
+    print("Transcrição concluida!")
+
+    return texto
+
+def main():
+    audio = encontrar_audio()
+
+    texto = transcrever_audio(audio)
+
+    print("\n---- TRANSCRIÇÂO ----\n")
+    print(texto[:1000])
+
+
+if __name__ == "__main__":
+    main()
